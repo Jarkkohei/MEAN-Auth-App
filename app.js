86582e08a69ea70924569e8b1657d5e0d1cb383b
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-12-09 14:27:46 +0200

    Situation after the part 2

--- app.js
+++ app.js
@@ -4,15 +4,43 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//  This replaces the depracated "mongoose.connect(config.database);" -command
+mongoose.createConnection(config.database, { useMongoClient: true });
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Database error ' + err);
+});
 
 const app = express();
 
+const users = require('./routes/users');
+
+//  Port number
 const port = 3000;
 
+//  CORS middleware
+app.use(cors());
+
+//  Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//  Bodyparser middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+//  Index route
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 });
 
+//  Start server
 app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
